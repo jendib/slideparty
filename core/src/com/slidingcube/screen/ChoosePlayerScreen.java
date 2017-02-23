eb4136b8d3125c8952cd1da42f17f59ea0e27d8a
Unknown <damien.troncy@gmail.com>
2017-02-23 14:05:11 +0100

    Change texture of pressed button

--- core/src/com/slidingcube/screen/ChoosePlayerScreen.java
+++ core/src/com/slidingcube/screen/ChoosePlayerScreen.java
@@ -118,9 +118,13 @@ public class ChoosePlayerScreen extends BaseScreen {
         playBtnImage.addListener(new InputListener() {
             public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
 
-                game.setScreen(new GameScreen(game, numberOfPlayer));
+                playBtnImage.setChecked(true);
 
                 return true;
+            }
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button)
+            {
+                game.setScreen(new GameScreen(game, numberOfPlayer));
             }
         });
     }
